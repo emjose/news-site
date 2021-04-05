8383fe8c43934a3dab717a027847fc67f915971c
emjose <emjose@gmail.com>
2021-04-05 12:08:11 -0400

    page setup

---
+++ pages/eom.js
@@ -0,0 +1,17 @@
+export const EOM = ({ employee }) => {
+    return(
+        <div className="page-container">
+            <div>
+                <h1>Employee of the Month</h1>
+            </div>
+        </div>
+    )
+};
+
+// export const getServerSideProps = async pageContext => {
+//     const apiResponse = await fetch(
+//         ''
+//     )
+// }
+
+export default EOM;
