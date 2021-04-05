ff7104e7cde9453965d1aec4ede51fbd2313cbe1
emjose <emjose@gmail.com>
2021-04-05 13:36:15 -0400

    adding pages and toolbar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from 'next/head'
+import { Toolbar } from '../components/toolbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (<>hello world</>);
+  return (
+    <div className='page-container'>
+
+    <Toolbar/>
+
+      <div className={styles.main}>
+        <h1>News Site</h1>
+
+        <h3>Your one stop show for the Latest News</h3>
+      </div>
+    </div>
+
+  );
 }
 
 // Emmanuel Jose logo and favicon. © Emmanuel Jose. All Rights Reserved.
