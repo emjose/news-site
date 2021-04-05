0835b6f0cb02a726ce419815fe26e1c588f0a40e
emjose <emjose@gmail.com>
2021-04-05 13:40:41 -0400

    url blank target

--- components/toolbar.js
+++ components/toolbar.js
@@ -9,7 +9,11 @@ export const Toolbar = () => {
             <div onClick={() => router.push('/')}>Home</div>
             <div onClick={() => router.push('/feed/1')}>Feed</div>
             <div onClick={() => router.push('/eom')}>EOM</div>
-            <div onClick={() => window.location.href = 'https://twitter.com/Emmanuel_Labor'}>Twitter</div>
+            <div onClick={() => window.open('https://twitter.com/Emmanuel_Labor','_blank')}>Twitter</div>
         </div>
     )
 };
+
+// setting target blank to a JavaScript window location (change window.location to window.open with parameters of URL and target)
+
+// https://stackoverflow.com/questions/18476373/how-to-add-target-blank-to-javascript-window-location
